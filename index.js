8240fd396743c3a5d919b82be73eb90ebc08ce7b
Julian beck <ju-fa-beck@t-online.de>
2018-07-06 16:12:50 +0900

    added extra method, for tests

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const isWindows = require("is-windows");
-const upath  = require("upath");
+const upath = require("upath");
+const opn = require('opn');
 
 module.exports = (path) => {
-    if (isWindows)
-        return upath.normalize(path).replace(/\//g,"\\");
+
+    if (isWindows())
+        return changeBackslash(path);
     else return path;
-    
+
+}
+module.exports.test = (path) => {
+    return changeBackslash(path);
+};
+
+function changeBackslash(path) {
+    return upath.normalize(path).replace(/\//g, "\\");
 }
--- test.js
+++ test.js
@@ -2,15 +2,12 @@ const expect = require("chai").expect;
 const chai = require("chai");
 const m = require(".");
 const opn = require('opn');
-
+const isWindows = require("is-windows");
+const upath  = require("upath");
 
 
 describe('test', async () => {
-    it('', async () => {
+    it("test", async () => {
         let pa = m("node_modules/chai");
-        opn(pa).then(() => {
-            // image viewer closed
-        });
-
-    });
+        console.log(JSON.stringify(pa))    });
 });
